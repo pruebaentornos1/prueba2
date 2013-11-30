5a4a60ee6e3fb196c07882ad7ced4e473138a666
matevic <matevic@192.168.2.112>
2013-11-30 22:28:09 +0100

    hola tio

---
+++ PruebaEntornos/src/NewMain.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author matevic
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num=100; //Declara una variable de tipo entero cuyo nombre es num y su valor inicial 100
+        System.out.println("El valor de num es "+num);
+        num=num*2;
+        System.out.println("El doble es: "+num);
+    }
+    
+}
