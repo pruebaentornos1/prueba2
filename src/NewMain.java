dd7b603a9cc39f0db8ac85109bc7dfe0f52d2345
matevic <matevic@192.168.2.112>
2013-11-30 23:02:24 +0100

    CAMBIOS NUEVOS

--- src/NewMain.java
+++ src/NewMain.java
@@ -14,7 +14,9 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Prueba");
-    }
+        System.out.println("rueba"); 
+        ////jADFPISAOJDGNDOPFUBNDFPJBDNFIUBNDF
+        //APDFJOBNADPFIJNB
+                    }
     
 }
